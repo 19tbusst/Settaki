d33dc12699fdcd815c12cbde85fc9f6add190d2d
Tom Busst <19tbusst@wakatipu.school.nz>
2021-03-03 21:48:24 +0000

    added .decide, .roll and updated .ping

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from discord.ext import commands
 import os
 
@@ -6,21 +7,30 @@ client = commands.Bot(command_prefix=".")
 token = os.getenv("DISCORD_BOT_TOKEN")
 
 @client.event
-async def on_ready() :
+async def on_ready():
     await client.change_presence(status = discord.Status.online, activity = discord.Game("Use .help for a list of commands"))
     print("I am online")
 
-@client.command()
-async def ping(ctx) :
-    await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
+@client.command
+async def ping(ctx):
+	await ctx.send(f"Pong {round(client.latency)}ms")
 
-@client.command(name="whoami")
-async def whoami(ctx) :
-    await ctx.send(f"You are {ctx.message.author.name}")
+@client.command
+async def roll(ctx):
+	await ctx.send(f"You rolled {random.randint(1,6)}")
 
-@client.command()
-async def clear(ctx, amount=3) :
-    await ctx.channel.purge(limit=amount)
+@client.command
+async def decide(ctx, args*):
+	times = 0
+	decision = random.randint(1, len(args))
+	for i in args:
+		times += 1
+		if times == decision:
+			ctx.send(i)
+
+
+		
+
 
 #webhook
 
